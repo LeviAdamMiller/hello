9ba7126d15859cf6e97b77a6a211820f17f3c146
Levi Miller <Miller.Levi@student.greenriver.edu>
2024-01-11 13:46:01 -0500

    adding index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+init_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once ('vendor/autoload.php');
+
+// instantiate the F3 Base class
+$F3 = Base::instance();
+//Base F3 = new Base();
+
