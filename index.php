be71f27ec0333155d1da4d0b95b5587c0df5954c
Levi Miller <Miller.Levi@student.greenriver.edu>
2024-01-14 17:51:52 -0500

    added fat free

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
+// This is my controller
 
-
-echo "Hello";
-
-init_set('display_errors', 1);
+// turn on error reporting
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//require the autoload
 require_once ('vendor/autoload.php');
 
 // instantiate the F3 Base class
-$F3 = Base::instance();
+$f3 = Base::instance();
 //Base F3 = new Base();
+
+//define a default route
+$f3 ->route('GET /', function (){
+    echo "<h1>Hello!</h1>";
+});
+
+// run fat free
+$f3->run();
+
 
